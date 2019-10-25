457b78673001ab34fe0b9c29ada5dc4180cb98df
BaptisteBrs <baptiste.boursin@gmail.com>
2019-10-25 13:54:48 +0200

    first commit

---
+++ projet_poker_proba_eleves.py
@@ -0,0 +1,379 @@
+import time
+
+class Card :
+    """ Classe représentant les cartes d'un jeu classique de 54 cartes.
+    """
+    VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    COLORS = ["\u2665","\u2660","\u2663","\u2666"] # Hearts,Spades,Clubs,Diamonds
+
+    def __init__(self,value,color) :
+        self.value=value
+        self.color=color
+
+    def __str__(self) :
+        """ Fonction pour la représentation graphique """
+        return self.VALUES[self.value]+self.COLORS[self.color]
+
+    def get_remaining_cards(used_cards) :
+        """ Retourne l'ensemble des cartes sauf celles présentes dans l'ensemble used_cards
+        """
+        res=[]
+        for i in range(0,13) :
+            for j in range(0,4) :
+                c = Card(i,j)
+                if not(c in used_cards) :
+                    res.append(Card(i,j))
+        return res
+
+    """ Fonctions pour la comparaison des cartes (utilisées dans la gestion des mains)
+    """
+    def __lt__(self, other)  : # For x < y
+        return self.value < other.value or (self.value == other.value and self.color < other.color)
+    def __le__(self, other)  : # For x <= y
+        return self.value < other.value or (self.value == other.value and self.color <= other.color)
+    def __eq__(self, other)  : # For x == y
+        return self.value == other.value and self.color == other.color
+    def __ne__(self,other) : # For x != y
+        return not(self == other)
+    def __gt__(self, other)  : # For x > y
+        return not(self <= other)
+    def __ge__(self, other)  : # For x >= y
+        return not(self < other)
+
+class HoleCards :
+    """ Classe représentant les 2 cartes du joueur
+    """
+
+    def __init__(self,card1,card2) :
+        if card1 > card2 :
+            self.card1=card1
+            self.card2=card2
+        else :
+            self.card1=card2
+            self.card2=card1
+
+    def __str__(self) :
+        """ Fonction pour la représentation graphique """
+        return "("+str(self.card1)+","+str(self.card2)+")"
+
+class CardValue :
+    """ Classe interne pour la comparaison des mains
+    """
+    def __init__(self,number,value) :
+        self.number = number
+        self.value = value
+
+    def __lt__(self, other)  : # For x < y
+        return self.number < other.number or (self.number == other.number and self.value < other.value)
+    def __le__(self, other)  : # For x <= y
+        return self.number < other.number or (self.number == other.number and self.value <= other.value)
+    def __eq__(self, other)  : # For x == y
+        return self.value == other.value and self.number == other.number
+    def __ne__(self,other) : # For x != y
+        return not(self == other)
+    def __gt__(self, other)  : # For x > y
+        return not(self <= other)
+    def __ge__(self, other)  : # For x >= y
+        return not(self < other)
+
+class Hand :
+    """ Classe représentant une main (5 cartes)
+    """
+    def __init__(self,cards) :
+        self.cards = cards
+        self.cards.sort()
+
+    def is_all_same_color(self) :
+        """ Fonction interne """
+        c = self.cards[0].color
+        for i in range(1,5) :
+            if self.cards[i].color != c :
+                return False
+        return True
+
+    def is_following_values(self) :
+        """ Fonction interne """
+        ## test if straight with Ace after King
+        straight_ace_after_king = self.cards[0].value==0
+        for i in range(1,5) :
+            if self.cards[i].value != (8+i) :
+                straight_ace_after_king = False
+                break
+        if straight_ace_after_king :
+            return True
+        ## others cases
+        for i in range(1,5) :
+            if self.cards[i].value != (self.cards[i-1].value+1) :
+                return False
+        return True
+
+    def get_cards_values(self) :
+        """ Fonction interne """
+        values = [0]*14
+        res=[]
+        for c in self.cards :
+            values[(c.value+13)%14]+=1
+        for i in range(1,14) :
+            if values[i]!=0 :
+                res.append(CardValue(values[i],i))
+        res.sort(reverse=True)
+        return res
+
+    def get_St_value(self) :
+        """ Fonction interne """
+        if self.cards[0].value == 0 and self.cards[4].value==12 :
+            return 13
+        else :
+            return self.cards[4].value
+
+    def get_hand_value(self) :
+        """ Fonction interne """
+        if self.is_straight_flush() :
+            return (9,self.get_St_value())
+        if self.is_four_of_a_kind() :
+            return (8,self.get_cards_values())
+        if self.is_full_house() :
+            return (7,self.get_cards_values())
+        if self.is_flush() :
+            return (6,self.get_cards_values())
+        if self.is_straight() :
+            return (5,self.get_cards_values())
+        if self.is_three_of_a_kind() :
+            return (4,self.get_cards_values())
+        if self.is_two_pairs() :
+            return (3,self.get_cards_values())
+        if self.is_pair() :
+            return (2,self.get_cards_values())
+        else :
+            return (1,self.get_cards_values())
+
+    def is_straight_flush(self) :
+        """ teste si la main est une quinte flush """
+        return self.is_all_same_color() and self.is_following_values()
+
+    def is_flush(self) :
+        """ teste si la main est une couleur """
+        return self.is_all_same_color() and not(self.is_following_values())
+
+    def is_straight(self) :
+        """ teste si la main est une suite """
+        return not(self.is_all_same_color()) and self.is_following_values()
+
+    def is_four_of_a_kind(self) :
+        """ teste si la main est un carré """
+        values = [0]*13
+        for c in self.cards :
+            values[c.value]+=1
+        for i in values :
+            if i==4 :
+                return True
+        return False
+
+    def is_full_house(self) :
+        """ teste si la main est un full """
+        values = [0]*13
+        for c in self.cards :
+            values[c.value]+=1
+        pair = False
+        three_of_a_kind = False
+        for i in values :
+            if i==3 :
+                three_of_a_kind = True
+            if i==2 :
+                pair = True
+        return pair and three_of_a_kind
+
+    def is_three_of_a_kind(self) :
+        """ teste si la main est un brelan """
+        values = [0]*13
+        for c in self.cards :
+            values[c.value]+=1
+        three_of_a_kind = False
+        for i in values :
+            if i==3 :
+                three_of_a_kind = True
+            if i==2 :
+                return False
+        return three_of_a_kind
+
+    def is_two_pairs(self) :
+        """ teste si la main est deux paires """
+        values = [0]*13
+        for c in self.cards :
+            values[c.value]+=1
+        pair = False
+        for i in values :
+            if i==2 :
+                if pair :
+                    return True
+                else :
+                    pair = True
+        return False
+
+    def is_pair(self) :
+        """ teste si la main est une paire """
+        values = [0]*13
+        for c in self.cards :
+            values[c.value]+=1
+        pair = False
+        for i in values :
+            if i==3 :
+                return False
+            if i==2 :
+                if pair :
+                    return False
+                else :
+                    pair = True
+        return pair
+
+    def __str__(self) :
+        res = "("+str(self.cards[0])+","+str(self.cards[1])+","+str(self.cards[2])+","
+        res+= str(self.cards[3])+","+str(self.cards[4])+")"
+        return res
+
+    def __lt__(self,other) :
+        """ teste si la main est < other """
+        if other == None :
+            return False
+        a,b = self.get_hand_value()
+        aa,bb = other.get_hand_value()
+        return a<aa or (a==aa and b < bb)
+
+    def __le__(self,other) :
+        """ teste si la main est <= other """
+        if other == None :
+            return False
+        a,b = self.get_hand_value()
+        aa,bb = other.get_hand_value()
+        return a<aa or (a==aa and b <= bb)
+
+    def __eq__(self,other) :
+        """ teste si la main est == other """
+        if other == None :
+            return False
+        a,b = self.get_hand_value()
+        aa,bb = other.get_hand_value()
+        return a==aa and b == bb
+
+    def __ne__(self,other) :
+        """ teste si la main est != other """
+        return not(self == other)
+
+    def __gt__(self, other)  :
+        """ teste si la main est > other """
+        return not(self <= other)
+
+    def __ge__(self, other)  :
+        """ teste si la main est >= other """
+        return not(self < other)
+
+class GameSituation :
+    """ Classe représentant l'état du jeu :
+        - hole_cards        : représente les cartes cachées du joueur
+        - community_cards   : représente les cartes communes sur la table
+    """
+    def __init__(self,hole_cards,community_cards) :
+        self.hole_cards = hole_cards
+        self.community_cards = community_cards
+
+    def get_best_hand(self) :
+        """ Fonction retournant la meilleure main possible parmi les 7 cartes : 2 du joueur et 5 communes
+            ATTENTION ! : à n'utiliser que s'il y a effectivement 5 cartes communes
+        """
+        if len(self.community_cards) != 5 :
+            raise NameError("get_best_hand")
+        cards_set = [self.hole_cards.card1,self.hole_cards.card2]
+        cards_set.extend(self.community_cards)
+        best_hand = None
+        for i in range(0,len(cards_set)-1) :
+            for j in range(i+1,len(cards_set)) :
+                c_s = cards_set[0:i]
+                c_s.extend(cards_set[i+1:j])
+                c_s.extend(cards_set[j+1:len(cards_set)])
+                h = Hand(c_s)
+                if h > best_hand :
+                    best_hand = h
+        return best_hand
+
+    def get_probabilities_after_river(self) :
+        float a,b,c;
+        for i in range (0,)
+
+        """ À compléter !
+            Fonction retournant 3 nombres réels (a,b,c) où
+            - a : représente la probabilité que le joueur gagne contre un autre joueur après la river
+            - b : représente la probabilité que le joueur fasse nul contre un autre joueur après la river
+            - c : représente la probabilité que le joueur perdre contre un autre joueur après la river
+        """
+        return 0,0,0
+
+    def get_probabilities_after_turn(self) :
+        float a,b,c;
+        """ À compléter !
+            Fonction retournant 3 nombres réels (a,b,c) où
+            - a : représente la probabilité que le joueur gagne contre un autre joueur après le turn
+            - b : représente la probabilité que le joueur fasse nul contre un autre joueur après le turn
+            - c : représente la probabilité que le joueur perdre contre un autre joueur après le turn
+        """
+        return 0,0,0
+
+    def get_probabilities_after_flop(self) :
+        float a,b,c;
+
+        """ À compléter !
+            Fonction retournant 3 nombres réels (a,b,c) où
+            - a : représente la probabilité que le joueur gagne contre un autre joueur après le flop
+            - b : représente la probabilité que le joueur fasse nul contre un autre joueur après le flop
+            - c : représente la probabilité que le joueur perdre contre un autre joueur après le flop
+        """
+        return 0,0,0
+
+    def get_probabilities(self) :
+        n = len(self.community_cards)
+        if n == 5 :
+            return self.get_probabilities_after_river()
+        elif n == 4 :
+            return self.get_probabilities_after_turn()
+        elif n == 3 :
+            return self.get_probabilities_after_flop()
+
+    def __str__(self) :
+        """ Fonction pour la représentation graphique """
+        res = "HC : "+str(self.hole_cards)+" , CC = ("
+        for c in self.community_cards :
+            res+=str(c)+","
+        return res[:-1]+")"
+
+
+h1=Hand([Card(0,1),Card(9,1),Card(10,1),Card(11,1),Card(12,1)])
+h2=Hand([Card(3,2),Card(3,3),Card(3,0),Card(3,1),Card(6,1)])
+h3=Hand([Card(3,2),Card(3,1),Card(3,0),Card(7,1),Card(7,3)])
+h4=Hand([Card(1,2),Card(3,2),Card(8,2),Card(9,2),Card(0,2)])
+h5=Hand([Card(5,2),Card(6,3),Card(7,0),Card(9,1),Card(8,1)])
+h6=Hand([Card(6,2),Card(6,3),Card(6,0),Card(3,1),Card(10,1)])
+h7=Hand([Card(10,2),Card(10,3),Card(12,0),Card(12,1),Card(4,1)])
+h8=Hand([Card(0,2),Card(0,3),Card(2,0),Card(8,1),Card(9,1)])
+h9=Hand([Card(1,2),Card(5,3),Card(6,0),Card(8,1),Card(11,1)])
+
+
+hands = [h1,h2,h3,h4,h5,h6,h7,h8,h9]
+
+print("Est-ce que "+str(h1)+" > "+str(h2)+" : "+str(h1 > h2))
+print("Est-ce que "+str(h3)+" == "+str(h4)+" : "+str(h3 == h4))
+print("Est-ce que "+str(h5)+" >= "+str(h6)+" : "+str(h5 >= h6))
+print("Est-ce que "+str(h7)+" < "+str(h8)+" : "+str(h7 < h8))
+print("Est-ce que "+str(h9)+" != "+str(h1)+" : "+str(h9 != h1))
+
+
+h_c = HoleCards(Card(1,0),Card(4,1))
+c_c = [Card(3,3),Card(2,1),Card(5,3),Card(10,2),Card(11,1)]
+
+gs = GameSituation(h_c,c_c)
+print(gs)
+
+best_hand = gs.get_best_hand()
+print("Best hand : "+str(best_hand))
+
+start_time = time.time()
+print(gs.get_probabilities())
+print("--- %s seconds ---" % (time.time() - start_time))
---
+++ projet_poker_stats_eleves.py
@@ -0,0 +1,80 @@
+from scipy.stats import *
+import numpy as np
+import csv
+
+""" On représente les classes de valeurs en utilisant la borne sup de chaque intervalle :
+    les classes [0,0.3], ]0.3,0.6], ]0.6,0.8], ]0.8,1] sont représentées par [0.3,0.6,0.8,1]
+"""
+categories = [0.3,0.6,0.8,1]
+
+class Profile :
+    """ Classe représentant le profil d'un joueur
+    """
+    def __init__(self,actions_stats_csv,name) :
+        self.name = name
+        self.actions_stats = {}
+        for c in categories :
+            self.actions_stats[str(c)] = {}
+        with open(actions_stats_csv, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader :
+                r = dict(row)
+                self.actions_stats[r['hand_value']][r['action']]=float(r['value'])
+        csvfile.close()
+    def get_frequencies_from_category(self,cat) :
+        """ Fonction retournant les fréquences de coups du profil pour la classe de valeurs cat
+        """
+        return np.array([self.actions_stats[str(cat)]['Fold'],self.actions_stats[str(cat)]['Check'],self.actions_stats[str(cat)]['Call'],self.actions_stats[str(cat)]['Raise']])
+
+class Actions :
+    """ Classe pour le traitement des actions d'un joueur :
+        recorded_actions : dictionnaire contenant les différents coups joué par le joueur
+    """
+    def __init__(self,actions_csv) :
+        self.recorded_actions = {}
+        for x in categories :
+            self.recorded_actions[str(x)]={'Fold':0,'Check':0,'Call':0,'Raise':0}
+        with open(actions_csv, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader :
+                r = dict(row)
+                self.recorded_actions[self.get_category(r['hand_value'])][r['action']]+=1
+        csvfile.close()
+
+    def get_category(self,x) :
+        """ fonction interne
+        """
+        for y in categories :
+            if float(x) < y :
+                return str(y)
+        return "No catetory"
+
+    def get_numbers_from_category(self,cat) :
+        """ fonction retournant le tableau des effectifs pour la classe de valeur cat
+        """
+        return np.array([self.recorded_actions[str(cat)]['Fold'],self.recorded_actions[str(cat)]['Check'],self.recorded_actions[str(cat)]['Call'],self.recorded_actions[str(cat)]['Raise']])
+
+    def get_best_profile(self,profiles) :
+        res = "Nobody"
+        return res
+
+agg_serr = Profile("actions_agressif_serre.csv","Agressif/serré")
+agg_larg = Profile("actions_agressif_large.csv","Agressif/large")
+pass_serr = Profile("actions_passif_serre.csv","Passif/serré")
+pass_larg = Profile("actions_passif_large.csv","Passif/large")
+
+profiles = [agg_larg,agg_serr,pass_larg,pass_serr]
+
+actions_joueurA = Actions("joueurA.csv")
+actions_joueurB = Actions("joueurB.csv")
+actions_joueurC = Actions("joueurC.csv")
+actions_joueurD = Actions("joueurD.csv")
+actions_joueurE = Actions("joueurE.csv")
+actions_joueurF = Actions("joueurF.csv")
+
+print("Joueur A : "+actions_joueurA.get_best_profile(profiles))
+print("Joueur B : "+actions_joueurB.get_best_profile(profiles))
+print("Joueur C : "+actions_joueurC.get_best_profile(profiles))
+print("Joueur D : "+actions_joueurD.get_best_profile(profiles))
+print("Joueur E : "+actions_joueurE.get_best_profile(profiles))
+print("Joueur F : "+actions_joueurF.get_best_profile(profiles))
